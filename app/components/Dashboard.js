431cb5cb81623aad182f3e2cf5f26e76bdea6b18
codemaster94 <newuser89@mailinator.com>
2017-10-09 22:35:36 +0530

    Create Dashboard.js

---
+++ app/components/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Dashboard () {
+  return (
+    <article>
+      <section className='text-section'>
+        <h1>Dashboard</h1>
+        <p>
+          Welcome, you are logged in! 
+        </p>
+      </section>
+    </article>
+  )
+}
+
+export default Dashboard
